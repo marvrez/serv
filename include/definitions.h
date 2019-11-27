a1e1d8677e32ba66fb2459cdb8f55c2f68008227
Marvin Reza <marvinreza@outlook.com>
2019-11-28 00:22:07 +0100

    setup definitions for http 1.1 and basic socket connection

---
+++ include/definitions.h
@@ -0,0 +1,57 @@
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#define SERV_VERSION "1.0"
+
+#include <inttypes.h>
+#include <unistd.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+#ifdef _SC_NPROCESSORS_ONLN
+#define NUM_THREADS sysconf(_SC_NPROCESSORS_ONLN)
+#else
+#define NUM_THREADS 4
+#endif
+
+typedef enum {
+    UNSUPPORTED=-1,
+    GET=1,
+    HEAD=1,
+} http_method;
+
+#define HTTP_1_1_VERSION "HTTP/1.1"
+#define HTTP_OK_HEADER "HTTP/1.1 200 OK\r\n"
+#define HTTP_CACHE_HEADERS "Server: serv/" SERV_VERSION "\r\nCache-control: no-cache, no-store, must-revalidate\r\nExpires: 0\r\nPragma: no-cache\r\n"
+#define HTTP_CONTENT_TYPE_KEY "Content-Type: "
+#define HTTP_CONTENT_LENGTH_KEY "Content-Length: "
+#define HTTP_DATE_KEY "Date: "
+#define HTTP_NEWLINE "\r\n"
+#define HTTP_END_HEADER HTTP_NEWLINE HTTP_NEWLINE
+
+#define BAD_REQUEST_HEADERS "HTTP/1.1 400 BAD REQUEST\r\nServer: serv/" SERV_VERSION "\r\nContent-Type: text/html\r\nContent-Length: 59\r\n"
+#define BAD_REQUEST_BODY "<html><body>\n<h1>Invalid HTTP request!</h1>\n</body></html>\n"
+#define NOT_FOUND_HEADERS "HTTP/1.1 404 NOT FOUND\r\nServer: serv/" SERV_VERSION "\r\nContent-Type: text/html\r\nContent-Length: 53\r\n"
+#define NOT_FOUND_BODY "<html><body>\n<h1>File not found!</h1>\n</body></html>\n"
+#define METHOD_NOT_SUPPORTED_HEADERS "HTTP/1.1 501 NOT IMPLEMENTED\r\nServer: serv/" SERV_VERSION "\r\nContent-Type: text/html\r\nContent-Length: 55\r\n"
+#define METHOD_NOT_SUPPORTED_BODY "<html><body>\n<h1>Method not supported!</h1>\n</body></html>\n"
+#define VERSION_NOT_SUPPORTED_HEADERS "HTTP/1.1 505 VERSION NOT SUPPORTED\r\nServer: serv/" SERV_VERSION "\r\nContent-Type: text/html\r\nContent-Length: 63\r\n"
+#define VERSION_NOT_SUPPORTED_BODY "<html><body>\n<h1>HTTP version must be 1.1!</h1>\n</body></html>\n"
+
+// 4kB
+#define TRANSFER_CHUNK_SIZE 1 << 15
+#define REQUEST_MAX_SIZE (4*TRANSFER_CHUNK_SIZE)
+
+#define BYTESET_TOKEN_END " \t\r\n"
+#define BYTESET_PATH_END "?#" BYTESET_TOKEN_END
+#define BYTESET_HEADER_KEY_END ":" BYTESET_TOKEN_END
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "definitions.h"
+
+// u16 port: input port to open connection on
+// returns: the file descriptor for the socket, -1 if error occurred
+s32 open_connection(u16 port)
+{
+    // make a listening socket
+    s32 sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1) {
+        fprintf(stderr, "failed to create socket\n");
+        return -1;
+    }
+
+    s32 reuse = 1; // allow for reuse of ports
+    if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1) {
+        fprintf(stderr, "failed to set socket options\n");
+        return -1;
+    }
+
+    // start listening on the socket.
+    struct sockaddr_in address = {};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if(bind(sock, (struct sockaddr*)&address, sizeof(address)) == -1) {
+        fprintf(stderr, "failed to bind socket\n");
+        return -1;
+    }
+    if(listen(sock, SOMAXCONN) == -1) {
+        fprintf(stderr, "failed to listen socket\n");
+        return -1;
+    }
+    return sock;
+}
 
 int main(int argc, char** argv)
 {
-    printf("kek\n");
+    u16 port = 4950;
+    if(argc > 1) {
+        int arg_port = atoi(argv[1]);
+        if(arg_port > 0) port = (u16)arg_port;
+    }
+    printf("spinning up server on port %d using %ld threads..\n", port, NUM_THREADS);
+
+    s32 sock = open_connection(port);
+    if(sock < -1) return 1;
+
+    printf("server is now listening.. \n");
+    while(1) {
+        s32 connection = accept(sock, 0, 0);
+        if(connection == -1) {
+            fprintf(stderr, "connection failed\n");
+            continue;
+        }
+    }
+
     return 0;
 }
